82c780b7d5ee9755260f1009aa8c49f4a2045ad6
pmckenna13 <patrickmckennaeng@gmail.comgit config --global user.emaail patrickmckennaeng@gmail.com>
2022-09-07 19:00:12 -0400

    First three exercises completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    const stringArray = string.split("");
+    let reverseStringArray = new Array(stringArray.length);
+    let result = "";
+    
+    for(let i=0; i < stringArray.length; i++)
+    {
+        reverseStringArray[i] = stringArray[stringArray.length -1 - i]
+    }
+
+    result = reverseStringArray.join("");
+
+    console.log(result);
+
+    return result; 
 };
 
 // Do not edit below this line
