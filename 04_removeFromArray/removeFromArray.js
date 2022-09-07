072ff51c8fac6fb1129e5a91268b226158da099d
pmckenna13 <patrickmckennaeng@gmail.comgit config --global user.emaail patrickmckennaeng@gmail.com>
2022-09-07 19:39:36 -0400

    Solution for removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removes) {
+
+    // declare filteredArray to originally be the same as the original array
+    
+    let filteredArray = array;
+
+    // loop through the elements to be removed and trim them from the filteredArray
+
+    for (let i=0; i < removes.length; i++)
+    {
+        filteredArray = filteredArray.filter(filteredArray => filteredArray !== removes[i]);
+    }
+
+    return filteredArray;
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
