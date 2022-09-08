42ca8f2a70f7d64456e5c65796895b75bd9b3453
pmckenna13 <patrickmckennaeng@gmail.comgit config --global user.emaail patrickmckennaeng@gmail.com>
2022-09-07 20:33:24 -0400

    Solution to tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const ftoc = function() {
+const ftoc = function(tempFaren) {
+  
+  let tempCelc = 0;
 
+  tempCelc = (tempFaren -32) * (5/9);
+  tempCelc = Math.round(tempCelc * 10) / 10;
+
+  return tempCelc;
 };
 
-const ctof = function() {
+const ctof = function(tempCelc) {
+
+  let tempFaren = 0;
+
+  tempFaren = (tempCelc * 9 / 5) + 32;
+  tempFaren = Math.round(tempFaren * 10) / 10;
+
+  return tempFaren;
 
 };
 
