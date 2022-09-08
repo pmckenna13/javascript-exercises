62135987d87774ee30a476a2857c04709ea212ff
pmckenna13 <patrickmckennaeng@gmail.comgit config --global user.emaail patrickmckennaeng@gmail.com>
2022-09-07 20:13:38 -0400

    Solution to sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,42 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let sum = 0;
+    let minNum = 0;
+    let maxNum = 0
+    let diff = 0;
+
+    if (num1 < 0
+        || num2 < 0
+        || typeof num1 != "number"
+        || typeof num2 != "number")
+    {
+        return "ERROR";
+    }
+
+    if (num1 > num2)
+    {
+        maxNum = num1;
+        minNum = num2;
+    }
+    else
+    {
+        maxNum = num2;
+        minNum = num1;
+    }
+
+    diff = maxNum - minNum;
+    
+    for(let i=0; i <=diff; i++)
+    {
+        sum += minNum + i;
+    }
+
+    if (diff == 0)
+    {
+        sum = minNum * 2;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
