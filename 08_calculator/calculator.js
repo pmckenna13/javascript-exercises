bcf02d82d149afbf0f49488496fd8e9e9f91c8fa
pmckenna13 <patrickmckennaeng@gmail.comgit config --global user.emaail patrickmckennaeng@gmail.com>
2022-09-22 19:36:47 -0400

    Solution to exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,57 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	var totalSum = 0;
+
+  for (i=0; i < nums.length; i++)
+  {
+    totalSum += Number(nums[i]);
+    
+  }
+
+  return totalSum;
+
+  
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+   var result = 1;
 
+   for (i = 0; i < nums.length; i++)
+   {
+      result *= Number(nums[i]);
+   }
+
+   return result;
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+  var result = 1;
+  
+  for(i =0; i < y; i++)
+  {
+    result = result * x;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+
+  var result = 0;
+
+  if(x == 0){
+    return 1;
+  }
+
+  return x * factorial(x-1);
+
 };
 
 // Do not edit below this line
